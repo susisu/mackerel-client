c8086b8554322c89685247c0485c33b49224b834
Susisu <susisu2413@yahoo.co.jp>
2024-05-03 15:38:29 +0900

    feat: add client for service APIs

---
+++ service.ts
@@ -0,0 +1,135 @@
+import { ApiClient } from "./api.ts";
+
+export type Service = {
+  name: string;
+  memo: string;
+  roles: string[];
+};
+
+export type CreateServiceInput = {
+  name: string;
+  memo: string;
+};
+
+export type Role = {
+  name: string;
+  memo: string;
+};
+
+export type CreateRoleInput = {
+  name: string;
+  memo: string;
+};
+
+export class ServiceClient {
+  private api: ApiClient;
+
+  constructor(api: ApiClient) {
+    this.api = api;
+  }
+
+  async list(options?: {
+    signal?: AbortSignal;
+  }): Promise<Service[]> {
+    const res = await this.api.fetch<{ services: Service[] }>(
+      "GET",
+      "/api/v0/services",
+      { signal: options?.signal },
+    );
+    return res.services;
+  }
+
+  async create(
+    input: CreateServiceInput,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<Service> {
+    const res = await this.api.fetch<Service>("POST", "/api/v0/services", {
+      body: JSON.stringify({
+        name: input.name,
+        memo: input.memo,
+      }),
+      signal: options?.signal,
+    });
+    return res;
+  }
+
+  async delete(
+    serviceName: string,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<Service> {
+    const res = await this.api.fetch<Service>(
+      "DELETE",
+      `/api/v0/services/${serviceName}`,
+      { signal: options?.signal },
+    );
+    return res;
+  }
+
+  async listRoles(
+    serviceName: string,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<Role[]> {
+    const res = await this.api.fetch<{ roles: Role[] }>(
+      "GET",
+      `/api/v0/services/${serviceName}/roles`,
+      { signal: options?.signal },
+    );
+    return res.roles;
+  }
+
+  async createRole(
+    serviceName: string,
+    input: CreateRoleInput,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<Role> {
+    const res = await this.api.fetch<Role>(
+      "POST",
+      `/api/v0/services/${serviceName}/roles`,
+      {
+        body: JSON.stringify({
+          name: input.name,
+          memo: input.memo,
+        }),
+        signal: options?.signal,
+      },
+    );
+    return res;
+  }
+
+  async deleteRole(
+    serviceName: string,
+    roleName: string,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<Role> {
+    const res = await this.api.fetch<Role>(
+      "DELETE",
+      `/api/v0/services/${serviceName}/roles/${roleName}`,
+      { signal: options?.signal },
+    );
+    return res;
+  }
+
+  async listMetricNames(
+    serviceName: string,
+    options?: {
+      signal?: AbortSignal;
+    },
+  ): Promise<string[]> {
+    const res = await this.api.fetch<{ names: string[] }>(
+      "GET",
+      `/api/v0/services/${serviceName}/metric-names`,
+      { signal: options?.signal },
+    );
+    return res.names;
+  }
+}
