710e175dbe4ecad84ef7524bea040af3185ec248
Susisu <susisu2413@yahoo.co.jp>
2024-05-05 16:28:25 +0900

    refactor: sort by name

--- mod.ts
+++ mod.ts
@@ -1,27 +1,29 @@
 import type { Options } from "./types.ts";
+
 import { ApiClient } from "./api.ts";
-import { HostsApiClient } from "./hosts.ts";
-import { ServicesApiClient } from "./services.ts";
-import { MetricsApiClient } from "./metrics.ts";
+
 import { GraphDefsApiClient } from "./graphDefs.ts";
+import { HostsApiClient } from "./hosts.ts";
+import { MetricsApiClient } from "./metrics.ts";
+import { ServicesApiClient } from "./services.ts";
 
 export type MackerelClientOptions = Options<{
   apiBase: string | URL;
 }>;
 
 export class MackerelClient {
-  readonly hosts: HostsApiClient;
-  readonly services: ServicesApiClient;
-  readonly metrics: MetricsApiClient;
   readonly graphDefs: GraphDefsApiClient;
+  readonly hosts: HostsApiClient;
+  readonly metrics: MetricsApiClient;
+  readonly services: ServicesApiClient;
 
   constructor(apiKey: string, options?: MackerelClientOptions) {
     const apiClient = new ApiClient(apiKey, {
       base: options?.apiBase,
     });
-    this.hosts = new HostsApiClient(apiClient);
-    this.services = new ServicesApiClient(apiClient);
-    this.metrics = new MetricsApiClient(apiClient);
     this.graphDefs = new GraphDefsApiClient(apiClient);
+    this.hosts = new HostsApiClient(apiClient);
+    this.metrics = new MetricsApiClient(apiClient);
+    this.services = new ServicesApiClient(apiClient);
   }
 }
