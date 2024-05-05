322f9d08bc73b8a62e9bddfe75f38c478c1ef61f
Susisu <susisu2413@yahoo.co.jp>
2024-05-05 16:12:10 +0900

    feat: add graphDefs API client

---
+++ graphDefs.ts
@@ -0,0 +1,50 @@
+import type { ApiClient, ApiOptions } from "./api.ts";
+
+export type CreateHostGraphDefsInputGraphDef = Readonly<{
+  name: string;
+  displayName?: string | undefined;
+  unit: GraphDefUnit;
+  metrics: readonly CreateHostGraphDefsInputGraphDefMetric[];
+}>;
+
+export type CreateHostGraphDefsInputGraphDefMetric = Readonly<{
+  name: string;
+  displayName?: string | undefined;
+  isStacked: boolean;
+}>;
+
+export type GraphDefUnit =
+  | "float"
+  | "integer"
+  | "percentage"
+  | "seconds"
+  | "milliseconds"
+  | "bytes"
+  | "bytes/sec"
+  | "bits/sec"
+  | "iops";
+
+export class GraphDefsApiClient {
+  private api: ApiClient;
+
+  constructor(api: ApiClient) {
+    this.api = api;
+  }
+
+  async createHostGraphDefs(
+    graphDefs: readonly CreateHostGraphDefsInputGraphDef[],
+    options?: ApiOptions,
+  ): Promise<void> {
+    await this.api.fetch<
+      { success: true },
+      readonly CreateHostGraphDefsInputGraphDef[]
+    >(
+      "POST",
+      "/api/v0/graph-defs/create",
+      {
+        body: graphDefs,
+        signal: options?.signal,
+      },
+    );
+  }
+}
--- mod.ts
+++ mod.ts
@@ -3,6 +3,7 @@ import { ApiClient } from "./api.ts";
 import { HostsApiClient } from "./hosts.ts";
 import { ServicesApiClient } from "./services.ts";
 import { MetricsApiClient } from "./metrics.ts";
+import { GraphDefsApiClient } from "./graphDefs.ts";
 
 export type MackerelClientOptions = Options<{
   apiBase: string | URL;
@@ -12,6 +13,7 @@ export class MackerelClient {
   readonly hosts: HostsApiClient;
   readonly services: ServicesApiClient;
   readonly metrics: MetricsApiClient;
+  readonly graphDefs: GraphDefsApiClient;
 
   constructor(apiKey: string, options?: MackerelClientOptions) {
     const apiClient = new ApiClient(apiKey, {
@@ -20,5 +22,6 @@ export class MackerelClient {
     this.hosts = new HostsApiClient(apiClient);
     this.services = new ServicesApiClient(apiClient);
     this.metrics = new MetricsApiClient(apiClient);
+    this.graphDefs = new GraphDefsApiClient(apiClient);
   }
 }
