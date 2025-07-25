86cbbe800b14c328fc3a0f334029f1b213313682
Susisu <susisu2413@yahoo.co.jp>
2025-07-25 14:07:30 +0900

    test: remove mock_fetch from dependencies

--- fetcher.test.ts
+++ fetcher.test.ts
@@ -1,32 +1,20 @@
 import { assertEquals, assertRejects } from "@std/assert";
-import { afterAll, afterEach, beforeAll, describe, it } from "@std/testing/bdd";
-import { assertSpyCalls, spy } from "@std/testing/mock";
-import * as mf from "mock_fetch";
+import { describe, it } from "@std/testing/bdd";
+import { assertSpyCalls, stub } from "@std/testing/mock";
 import { DefaultFetcher } from "./fetcher.ts";
 
 describe("DefaultFetcher", () => {
-  beforeAll(() => {
-    mf.install();
-  });
-
-  afterAll(() => {
-    mf.uninstall();
-  });
-
-  afterEach(() => {
-    mf.reset();
-  });
-
   describe("fetch", () => {
     it("can fetch without parameters and body", async () => {
-      const handler = spy((_req: Request) => new Response(JSON.stringify({ id: 42 })));
-      mf.mock("GET@/api/v0/dummy", handler);
+      using fetchStub = stub(globalThis, "fetch", async () => {
+        return await new Response(JSON.stringify({ id: 42 }));
+      });
 
       const fetcher = new DefaultFetcher("deadbeef");
       const res = await fetcher.fetch("GET", "/api/v0/dummy");
 
-      assertSpyCalls(handler, 1);
-      const req = handler.calls[0].args[0];
+      assertSpyCalls(fetchStub, 1);
+      const req = fetchStub.calls[0].args[0] as Request;
       assertEquals(req.method, "GET");
       assertEquals(req.url, "https://api.mackerelio.com/api/v0/dummy");
       assertEquals(req.headers.get("X-Api-Key"), "deadbeef");
@@ -36,16 +24,17 @@ describe("DefaultFetcher", () => {
     });
 
     it("can fetch with parameters", async () => {
-      const handler = spy((_req: Request) => new Response(JSON.stringify({ id: 42 })));
-      mf.mock("GET@/api/v0/dummy", handler);
+      using fetchStub = stub(globalThis, "fetch", async () => {
+        return await new Response(JSON.stringify({ id: 42 }));
+      });
 
       const fetcher = new DefaultFetcher("deadbeef");
       const res = await fetcher.fetch("GET", "/api/v0/dummy", {
         params: new URLSearchParams({ foo: "bar" }),
       });
 
-      assertSpyCalls(handler, 1);
-      const req = handler.calls[0].args[0];
+      assertSpyCalls(fetchStub, 1);
+      const req = fetchStub.calls[0].args[0] as Request;
       assertEquals(req.method, "GET");
       assertEquals(req.url, "https://api.mackerelio.com/api/v0/dummy?foo=bar");
       assertEquals(req.headers.get("X-Api-Key"), "deadbeef");
@@ -55,16 +44,17 @@ describe("DefaultFetcher", () => {
     });
 
     it("can fetch with body", async () => {
-      const handler = spy((_req: Request) => new Response(JSON.stringify({ id: 42 })));
-      mf.mock("POST@/api/v0/dummy", handler);
+      using fetchStub = stub(globalThis, "fetch", async () => {
+        return await new Response(JSON.stringify({ id: 42 }));
+      });
 
       const fetcher = new DefaultFetcher("deadbeef");
       const res = await fetcher.fetch("POST", "/api/v0/dummy", {
         body: { foo: "bar" },
       });
 
-      assertSpyCalls(handler, 1);
-      const req = handler.calls[0].args[0];
+      assertSpyCalls(fetchStub, 1);
+      const req = fetchStub.calls[0].args[0] as Request;
       assertEquals(req.method, "POST");
       assertEquals(req.url, "https://api.mackerelio.com/api/v0/dummy");
       assertEquals(req.headers.get("X-Api-Key"), "deadbeef");
@@ -75,16 +65,17 @@ describe("DefaultFetcher", () => {
     });
 
     it("allows to override the base URL", async () => {
-      const handler = spy((_req: Request) => new Response(JSON.stringify({ id: 42 })));
-      mf.mock("GET@/api/v0/dummy", handler);
+      using fetchStub = stub(globalThis, "fetch", async () => {
+        return await new Response(JSON.stringify({ id: 42 }));
+      });
 
       const fetcher = new DefaultFetcher("deadbeef", {
         base: "https://foo.example/",
       });
       const res = await fetcher.fetch("GET", "/api/v0/dummy");
 
-      assertSpyCalls(handler, 1);
-      const req = handler.calls[0].args[0];
+      assertSpyCalls(fetchStub, 1);
+      const req = fetchStub.calls[0].args[0] as Request;
       assertEquals(req.method, "GET");
       assertEquals(req.url, "https://foo.example/api/v0/dummy");
       assertEquals(req.headers.get("X-Api-Key"), "deadbeef");
@@ -94,12 +85,11 @@ describe("DefaultFetcher", () => {
     });
 
     it("rejects if API request was unsuccessful", async () => {
-      const handler = spy((_req: Request) =>
-        new Response(JSON.stringify({ error: "bad request" }), {
+      using _fetchStub = stub(globalThis, "fetch", async () => {
+        return await new Response(JSON.stringify({ error: "bad request" }), {
           status: 400,
-        })
-      );
-      mf.mock("GET@/api/v0/dummy", handler);
+        });
+      });
 
       const fetcher = new DefaultFetcher("deadbeef");
 
